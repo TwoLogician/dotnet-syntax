241a7663a5b8b414d4887e825dcb4be5bc97824d
wk-j <somnuk.wk@gmail.com>
2017-12-03 20:35:53 +0700

    Either

--- Match/Match.fsx
+++ Match/Match.fsx
@@ -1,12 +1,18 @@
-let (Some v1) = Some 11
-v1 = 11 |> printfn "%A"
+module A = 
+    let (Some v1) = Some 11
+    v1 = 11 |> printfn "%A"
 
 
-type ('a, 'b) Either = This of 'a | That of 'b
+module B = 
+    type ('a, 'b) Either = This of 'a | That of 'b
 
-let this10 = This 10
-let that10 = That 10 
+    let this10 = This 10
+    let that10 = That 10 
 
-let (This a | That a) = this10
+    let (This a | That a) = this10
 
-a |> printfn "%A" 
+    a |> printfn "%A" 
+
+module C = 
+    let [1],a,b,c = List.map id [1],11,12,13
+    (a,b,c) |> printfn "%A"
