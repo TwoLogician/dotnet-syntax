20296e802fcc036c2e2ba596d52fa18cf6b8e825
Somnuk Wongkhan <somnuk.wk@gmail.com>
2018-01-09 11:56:46 +0700

    Remove monadic

--- scripts/Async/Bind.fsx
+++ scripts/Async/Bind.fsx
@@ -6,15 +6,6 @@ let getMoreData a = async {
     return a + 1
 }
 
-async { 
-    let! a = getData()
-    let! b = getMoreData a
-    let! c = getMoreData b
-    let! d = getMoreData c
-    let! e = getMoreData d
-    return e
-}
-
 let (>>=) x f = async.Bind(x,f) 
 
 getData() >>= getMoreData >>= getMoreData >>= getMoreData  >>= getMoreData
