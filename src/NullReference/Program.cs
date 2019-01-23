1bd6191d01a815bec27f92c90998025999f62657
wk-j <somnuk.wk@gmail.com>
2019-01-23 15:54:53 +0700

    WarningsAsErrors

--- src/NullReference/Program.cs
+++ src/NullReference/Program.cs
@@ -4,14 +4,17 @@ namespace NullReference {
     class Program {
         static void Main(string[] args) {
             // string? s = null;
+            string s = null;
             // Console.WriteLine(s);
             string[] names = {
                 "Archimedes", "Pythagoras", "Euclid", "Socrates", "Plato"
             };
 
-            foreach (var name in names[1..4]) {
-                Console.WriteLine(name);
-            }
+            Console.WriteLine(s);
+
+            // foreach (var name in names[1..4]) {
+            //     Console.WriteLine(name);
+            // }
         }
     }
 }
