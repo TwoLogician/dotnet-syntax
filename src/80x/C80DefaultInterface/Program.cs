8d72242f0555ed3a7b7ce8ceec7b1cc9a2460d3f
wk-j <somnuk.wk@gmail.com>
2019-03-04 17:02:31 +0700

    Default interface methods

---
+++ src/80x/C80DefaultInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C80DefaultInterface {
+    interface IService {
+        void Start();
+        void Stop();
+        void Restart() {
+            Stop();
+            Start();
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
