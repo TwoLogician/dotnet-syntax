2ebe1e04e16d12979db5f090525c5c1cf83203a2
wk-j <somnuk.wk@gmail.com>
2019-03-04 17:11:54 +0700

    Restructured

---
+++ src/80x/C80TargetTypeNewExpressions/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace C80TargetTypeNewExpressions {
+    class Program {
+        static void Main(string[] args) {
+            //Point[] ps = { new (1, 4)};
+        }
+    }
+}
