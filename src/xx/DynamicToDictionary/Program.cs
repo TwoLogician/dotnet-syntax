843e4dcb4401765f5476914ed2b60b694d42f41e
wk-j <somnuk.wk@gmail.com>
2019-03-11 01:41:14 +0700

    Dictionary

---
+++ src/xx/DynamicToDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace DynamicToDictionary {
+    static class DynamicExtensions {
+        public static IDictionary<string, Object> ToDictionary(this object obj) {
+            var dict = TypeDescriptor.GetProperties(obj.GetType())
+                .OfType<PropertyDescriptor>().ToDictionary(p => p.Name, p => p.GetValue(obj));
+            return dict;
+        }
+
+    }
+    class Program {
+        static void Main(string[] args) {
+            var a = new {
+                A = 100,
+                B = 200,
+                C = 300
+            };
+
+            var dict = a.ToDictionary();
+            foreach (var item in dict) {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+        }
+    }
+}
