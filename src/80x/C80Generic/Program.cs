58ca96a12d2495ebd1841814a718d8a3131e5963
wk-j <somnuk.wk@gmail.com>
2019-06-18 11:14:19 +0700

    notnull

---
+++ src/80x/C80Generic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C80Generic {
+
+    class Program {
+        static void Main(string[] args) {
+        }
+
+        static void Go<T>(T data) where T : notnull {
+
+        }
+    }
+}
