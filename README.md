17a350ab9c450336cbd89acc9e98784fd78c0075
wk-j <somnuk.wk@gmail.com>
2019-08-23 00:45:22 +0700

    Indices

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## .NET Syntax
+
+[![Actions](https://github.com/wk-j/dotnet-syntax/workflows/dotnet/badge.svg)](https://github.com/wk-j/dotnet-syntax/actions)
---
+++ src/80x/C80Indices/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C80Indices {
+    class Program {
+        static void Main(string[] args) {
+            var data = new string[] {
+                "1",
+                "2"
+            };
+
+            var x = data[^1];
+            Console.WriteLine(x);
+        }
+    }
+}
