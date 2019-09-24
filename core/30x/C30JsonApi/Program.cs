9cfc5b3f6c9ed88b2f40dec1fdc09c118b5d8b85
wk-j <somnuk.wk@gmail.com>
2019-09-24 16:13:24 +0700

    New json serializer

---
+++ core/30x/C30JsonApi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+
+namespace C30JsonApi {
+    class O {
+        public int A { set; get; }
+        public int B { set; get; }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var a = new {
+                A = 100,
+                B = 200
+            };
+
+            var json = JsonSerializer.Serialize(a);
+            Console.WriteLine(json);
+
+            var obj = JsonSerializer.Deserialize<O>(json);
+            Console.WriteLine($"{obj.A} {obj.B}");
+        }
+    }
+}
