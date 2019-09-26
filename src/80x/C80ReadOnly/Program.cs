b2f70c4c0c7655861f26ee49602bcca44884a1fc
wk-j <somnuk.wk@gmail.com>
2019-09-26 15:33:39 +0700

    Read only

---
+++ src/80x/C80ReadOnly/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C80ReadOnly {
+    struct Person {
+        int a;
+
+        public Person(int a) {
+            this.a = a;
+        }
+
+        public readonly string A() {
+            // a = 200;
+            return "a";
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
