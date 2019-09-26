fee31a8e07ae39ce22c3449e13955a3e90a24e29
wk-j <somnuk.wk@gmail.com>
2019-09-26 19:42:30 +0700

    Properties pattern

---
+++ src/80x/C80PropertyPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C80PropertyPattern {
+    class Address {
+        public string State { set; get; }
+        public string Country { set; get; }
+    }
+
+    class Program {
+        static decimal Compute(Address location, decimal price) =>
+            location switch
+            {
+                { State: "WA", Country: "Thailand" } => price * 5M,
+                { State: "WA" } => price * 0.5M,
+                _ => 0M
+            };
+
+        static void Main(string[] args) {
+            var p1 = Compute(new Address { State = "WA" }, 10);
+            var p2 = Compute(new Address { State = "WA", Country = "Thailand" }, 10);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2)
+        }
+    }
+}
---
+++ src/80x/C80TuplePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C80TuplePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
