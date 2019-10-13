af56f77d26ae50f91979a2fda2f647d3f2cf44bb
wk-j <somnuk.wk@gmail.com>
2019-10-14 00:13:00 +0700

    Switch

--- src/80x/C80SwitchExpression/Program.cs
+++ src/80x/C80SwitchExpression/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 
 namespace C80SwitchExpression {
+
+    class Person {
+        public string Name { set; get; }
+    }
+
     class Program {
-        static void Main(string[] args) {
+        static void A() {
             var token = (true, true);
             var rs = token switch
             {
@@ -11,6 +16,20 @@ namespace C80SwitchExpression {
                 (false, true) => false,
                 (false, false) => false
             };
+        }
+
+        static string B(Person p) {
+            return p?.Name switch
+            {
+                "wk" => "Hello, wk",
+                _ => "?"
+            };
+
+        }
+
+        static void Main(string[] args) {
+            var a = B(null);
+            Console.WriteLine(a);
         }
     }
 }
