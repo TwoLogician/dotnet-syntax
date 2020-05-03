183809135b4dd9ca763869049f2dd4002398f4d6
wk <somnuk.wk@gmail.com>
2020-05-04 00:58:47 +0700

    Switch

--- src/80x/C80SwitchExpression/Program.cs
+++ src/80x/C80SwitchExpression/Program.cs
@@ -36,11 +36,23 @@ namespace C80SwitchExpression {
             return text2;
         }
 
+        static string F() {
+            var a = 1;
+            return a switch
+            {
+                1 => "ONE",
+                2 => "TWO",
+                3 => "THREE",
+                _ => throw new ArgumentOutOfRangeException()
+            } switch
+            { var c => c.ToLower() };
+        }
+
         static void Main(string[] args) {
             var a = B(null);
             var c = C(new Person { Name = "wk" });
-            Console.WriteLine(a);
-            Console.WriteLine(c);
+
+            Console.WriteLine(D());
         }
     }
 }
