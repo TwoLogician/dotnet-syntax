14158afbc77f9132453fdf8f7c242839ede9f96d
wk <somnuk.wk@gmail.com>
2020-05-07 19:49:51 +0700

    Program

--- src/80x/C80NullCoalescing/Program.cs
+++ src/80x/C80NullCoalescing/Program.cs
@@ -7,23 +7,17 @@ namespace C80NullCoalescing {
         static void A() {
             var isActive = true;
             var isDeleted = false;
-
-
-
-
-            var show = (isActive, isDeleted) is (true, false) ? true : false;
-
+            var show = (isActive, isDeleted) is (true, false) ? false : false;
             Console.WriteLine(show);
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] _) {
 
             List<int> a = null;
             a ??= new List<int>();
 
             List<int> x = null;
             A();
-
         }
 
     }
